4c4535b7d81f529f00e1f796350b7e79635e64b6
Paul P <85466221+pauljhp@users.noreply.github.com>
2024-03-11 10:10:59 +0800

    initial commit

--- set_environment_vars.sh
+++
@@ -1,7 +0,0 @@
-# /usr/bin/bash
-export OPENAI_API_KEY="d8a6014348be4cc7951de2428dd55594" && \
-    OPENAI_API_TYPE="azure" && \
-    OPENAI_API_VERSION="2023-07-01-preview" && \
-    AZURE_OPENAI_ENDPOINT="https://ipx-neo4j-openai-test.openai.azure.com/" && \
-    GOOGLE_CSE_ID="040ca56f062be4799" && \
-    GOOGLE_API_KEY="AIzaSyAzIpgVYFkbF-jcgVdIflJ5HeBJ-Jl9E7A"
