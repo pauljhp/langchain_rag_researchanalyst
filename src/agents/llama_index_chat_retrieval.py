0988b152ea59e675604b4e251bff15087072920b
ppipxam <p.peng@impaxam.com>
2024-04-18 23:49:00 +0800

    added google search

--- src/agents/llama_index_chat_retrieval.py
+++ src/agents/llama_index_chat_retrieval.py
@@ -1,11 +1,11 @@
 from query_engines import CustomQueryEngine, llm
-from tools.llamaindex_tools import duckduckgo_tools, arxiv_tools
+from tools.llamaindex_tools import duckduckgo_tools, arxiv_tools, google_search_tool
 from llama_index.core.agent import ReActAgent
 
 
 engine = CustomQueryEngine()
 query_tools = engine.get_query_engine_tools()
-tools = query_tools + duckduckgo_tools + arxiv_tools
+tools = query_tools + duckduckgo_tools + arxiv_tools + [google_search_tool]
 
 
 def get_new_agent():
--- src/tools/llamaindex_tools.py
+++ src/tools/llamaindex_tools.py
@@ -1,7 +1,28 @@
 from llama_index.tools.google.search.base import GoogleSearchToolSpec
 from llama_index.tools.duckduckgo.base import DuckDuckGoSearchToolSpec
 from llama_index.tools.arxiv.base import ArxivToolSpec
+from llama_index.core.tools.function_tool import FunctionTool, ToolMetadata
+from googleapiclient.discovery import build
+from pydantic import BaseModel
+import os
+
+
+def google_search(search_term):
+    service = build("customsearch", "v1", developerKey=os.environ.get("GOOGLE_API_KEY"))
+    res = service.cse().list(q=search_term, cx=os.environ.get("GOOGLE_CSE_ID")).execute()
+    return res['items']
+
+class GoogleSearchParams(BaseModel):
+    search_term: str
 
 
 duckduckgo_tools = DuckDuckGoSearchToolSpec().to_tool_list()
-arxiv_tools = ArxivToolSpec().to_tool_list()
+arxiv_tools = ArxivToolSpec(max_results=10).to_tool_list()
+google_search_tool = FunctionTool(
+    fn=google_search,
+    metadata=ToolMetadata(
+        name="google_search_tool",
+        description="Search for the latest information on google. Takes a string as input",
+        fn_schema=GoogleSearchParams
+    )
+)
